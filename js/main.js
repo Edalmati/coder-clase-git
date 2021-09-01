68aac06ec921392861a5fbe48fb719e960788a53
Edalmati <edalmati@gmail.com>
2021-09-01 13:06:07 -0300

    menu responsive

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// const nav = document.querySelector(".navMobile")
+
+// document.querySelector(".hamburger").addEventListener("click", e => {
+//   e.preventDefault()
+
+//   if (nav.classList.contains("open")) {
+//     nav.classList.remove("open")
+//   } else {
+//     nav.classList.add("open")
+//   }
+// })
